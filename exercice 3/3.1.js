d49f67360a637419e422e090c1c0a30d21be2388
Edouard de Romrée <deromree.edouard@gmail.com>
2022-11-24 16:58:54 +0100

    exercice 3 début

---
+++ exercice 3/3.1.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3,4,5];
+sum=0
+
+for (let elem of arr){
+    sum+= elem
+}
+
+console.log(sum)
+
+let arr1 = [101,102,103];
+sum=0
+
+for (let elem of arr1){
+    sum+= elem
+}
+console.log(sum)
---
+++ exercice 3/3.2.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5]
+numAverage=0
+for (let elem of arr){
+    numAverage+= elem
+}
+console.log(numAverage)
