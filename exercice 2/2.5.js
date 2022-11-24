606ce5324a8451febb53a6762ced10b799a9551a
Edouard de Romrée <deromree.edouard@gmail.com>
2022-11-24 15:57:52 +0100

    exercice 1 et 2

---
+++ exercice 2/2.5.js
@@ -0,0 +1,9 @@
+
+let i = Number(prompt("What is your number?"));
+
+while (i!==42){
+    let x = Number(prompt("Are you sure ?"));
+    if (x===42){
+     break;
+    }
+}
---
+++ exercice 2/2.7.js
@@ -0,0 +1,7 @@
+let n = Number(prompt("Enter a number"))
+sum=0;
+for(i=1;i<=n;i++){
+    x=Number(prompt("Enter a new number"))
+    sum+=x 
+    console.log(sum)
+}
